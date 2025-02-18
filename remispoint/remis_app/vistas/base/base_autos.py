eba65c23d97ef31ddfd53d48d0104fc74709384f
ifss13 <ianfrancosanchez@gmail.com>
2025-02-18 20:37:15 -0300

    bottom-navbar

--- remispoint/remis_app/vistas/base/base_autos.py
+++ remispoint/remis_app/vistas/base/base_autos.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect # type: ignore
+from django.shortcuts import render, redirect 
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
